65930d91ead6f4d95169d37c0b04cfe985a088c9
lrypto <lloydrebello@gmail.com>
2019-05-28 10:29:56 -0400

    using web3-react hook to get web3

--- src/components/test/MyTestDisplayComponent.tsx
+++ src/components/test/MyTestDisplayComponent.tsx
@@ -4,16 +4,36 @@ import { Store} from "../../common/Store";
 import { fetchSkills } from "../../common/Actions";
 import useChainState from "../chainstate/useChainState";
 import useAccountWatch from "../chainstate/useAccountWatch";
+import { useWeb3Context } from 'web3-react'
 
 
-
-
-function MyTestDisplayComponent(props: any ) {
+export default function MyTestDisplayComponent(props: any ) {
   const { state, dispatch} = React.useContext(Store);
   const {web3State, sChainState} = state;
+  
+
+  React.useEffect(() => {
+    context.setFirstValidConnector(['MetaMask'])
+  }, [])
+
+
+  const context = useWeb3Context();
+  if (!context.active && !context.error) {
+    // loading
+    //return ...
+    console.log("loading web3..")
+  } else if (context.error) {
+    //error
+    //return ...
+    console.error("something went wrong loading web3");
+  } else {
+    // success
+    console.log("web3 loaded");
+  }
+
 
   useChainState(web3State, sChainState, dispatch, fetchSkills);
-  useAccountWatch(web3State, dispatch, sChainState);
+  //useAccountWatch(web3State, dispatch, sChainState);
   
   const {skills} = state.skillsList;
 
@@ -22,8 +42,8 @@ function MyTestDisplayComponent(props: any ) {
       <p>skills: {skills}</p>
       <p>sos</p>
       <MyTestDisplayChild />
+      <p>web3 account:{context.account}</p>
     </div>
   );
 }
 
-export default MyTestDisplayComponent;
--- src/views/Test.tsx
+++ src/views/Test.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import MyTestDisplayComponent from '../components/test/MyTestDisplayComponent';
 
+import { Connectors } from 'web3-react';
+const { InjectedConnector, NetworkOnlyConnector } = Connectors;
+const MetaMask = new InjectedConnector({ supportedNetworks: [1,3,4] })
+const connectors = { MetaMask };
+import Web3Provider from 'web3-react';
+import Web3 from 'web3';
 
 
 export default function Test(props:any) {
@@ -12,9 +18,9 @@ export default function Test(props:any) {
           <div className="col-12">
             <h3 className="heading text-center">==--* test  +___=-`</h3>
             <div className="heading-underline"></div>
-           
-              <MyTestDisplayComponent />
-            
+            <Web3Provider connectors={connectors} libraryName={'web3.js'} web3Api={Web3}>
+                 <MyTestDisplayComponent />
+            </Web3Provider>
           </div>
         </div>
       </div>
