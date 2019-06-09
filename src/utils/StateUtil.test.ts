87123585af4268e58d101fb71adf7460c0e06d56
lrypto <lloydrebello@gmail.com>
2019-06-09 11:46:38 -0400

    stateutil test updated to invoke method vs. using typeof operator

--- src/utils/StateUtil.test.ts
+++ src/utils/StateUtil.test.ts
@@ -1,6 +1,26 @@
 import { resultToArray } from "./StateUtil";
-import { IUser } from "../common/Interfaces";
+import { IUser, IAppState } from "../common/Interfaces";
 //import puppeteer from "puppeteer";
+
+const testInput = [
+  {
+    "0": "firstmewser",
+    "1": "0xf7d1314C521E022a3992F63233C45A1B4c84e9fd",
+    "2": false,
+    name: "firstmewser",
+    ethAddr: "0xf7d1314C521E022a3992F63233C45A1B4c84e9fd",
+    isProvider: false
+  },
+  {
+    "0": "meeessssecond",
+    "1": "0x7504688b0de13d595b77F0346e7fa608ab93648e",
+    "2": false,
+    name: "meeessssecond",
+    ethAddr: "0x7504688b0de13d595b77F0346e7fa608ab93648e",
+    isProvider: false
+  }
+];
+
 
 test("empty array result", () => {
   const testInput = Array<any>();
@@ -9,29 +29,22 @@ test("empty array result", () => {
   expect(callResult).toEqual(expectedArray);
 });
 
-test("expected result to typed array", () => {
-  const testInput = [
-    {
-      "0": "firstmewser",
-      "1": "0xf7d1314C521E022a3992F63233C45A1B4c84e9fd",
-      "2": false,
-      name: "firstmewser",
-      ethAddr: "0xf7d1314C521E022a3992F63233C45A1B4c84e9fd",
-      isProvider: false
-    },
-    {
-      "0": "meeessssecond",
-      "1": "0x7504688b0de13d595b77F0346e7fa608ab93648e",
-      "2": false,
-      name: "meeessssecond",
-      ethAddr: "0x7504688b0de13d595b77F0346e7fa608ab93648e",
-      isProvider: false
-    }
-  ];
-
+test("expected result count from returned array", () => {
   const callResult = resultToArray<IUser>(testInput);
   expect(2).toBe(callResult.length);
 });
+
+test("expected result type returned array", () => {
+  const callResult = resultToArray<IUser>(testInput);
+  const expectedType = callResult[0].ethAddr !== undefined;
+  expect(expectedType).toBeTruthy();
+});
+ 
+
+
+
+
+
 
 /*
 test("That puppeteer runs", async () => {
